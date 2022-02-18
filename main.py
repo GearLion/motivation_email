afde97b25b8919278287f1088640f0078026b842
Judd Baroff <self@juddbaroff.com>
2022-02-18 11:44:31 -0600

    add functionality

---
+++ main.py
@@ -0,0 +1,16 @@
+import smtplib
+import datetime
+from random import choice
+from passwords import EMAIL, PASSWORD
+
+with open("quotes.txt") as quotes:
+    # This is a strange code format, as this keeps the file around until the day arrives. We should do the reverse.
+    if (datetime.datetime.now().weekday() + 2) % 7 == 5:
+        quotation = choice(quotes.readlines())
+        # After I've saved the quotation, I do not need to keep the txt file open either.
+        with smtplib.SMTP("smtp.gmail.com", port=587) as connection:
+            connection.starttls()
+            connection.login(user=EMAIL, password=PASSWORD)
+            connection.sendmail(from_addr=EMAIL, to_addrs="jjbaroff@gmail.com",
+                                msg=f"Subject: Monday Motivation Quotation\n\nWelcome to the newsletter,"
+                                    f"\n\n Here is today's quotation motivation:\n\n{quotation}.")
